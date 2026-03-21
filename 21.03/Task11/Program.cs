e89a91ba4806cf7db62d0e170cee05a1d1063026
adllall <adelinamamelnikova@yandex.ru>
2026-03-21 17:02:35 +0300

    21.03

---
+++ 21.03/Task11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static int[] ShiftLeft(int[] array, int k)
+    {
+        if (array == null || array.Length == 0 || k % array.Length == 0) return array.ToArray();
+            int n = array.Length;
+        int shift = k % n; 
+        return array.Skip(shift)
+            .Concat(array.Take(shift))
+            .ToArray();
+    }
+    public static void Main()
+    {
+     	int[] array = { 1, 2, 3, 4, 5 };
+    	int k = 4;
+    	var result = ShiftLeft(array, k);
+    	Console.WriteLine(string.Join(", ", result));
+    }
+		
+}
---
+++ 21.03/Task33/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+public class Task3
+{
+    public static void Main()
+    {
+        string[] strings = {  "pukkk", "privet", "blablabla", "kak", " ", "algosikikiki", "dela", "hahaha"};
+        var result = FindStringsWithMaxTwoRepeats(strings);
+        
+        foreach (var str in result)
+            Console.WriteLine(str);
+    }
+    
+    public static IEnumerable<string> FindStringsWithMaxTwoRepeats(IEnumerable<string> strings)
+    {
+        return strings
+            .Where(str => str != null)  
+            .Where(str => str.Length > 0) 
+            .Where(str => 
+                str.ToLower() 
+                    .GroupBy(c => c)
+                    .All(group => group.Count() <= 2)
+            );
+    }
+    
+    private static bool IsValidString(string str)
+    {
+        return str.GroupBy(c => c)
+            .All(group => group.Count() <= 2);
+    }
+}
